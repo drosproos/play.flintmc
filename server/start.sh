7e7c1605c8c998b3a2cf44ddc96dcd693a584f0c
drosproos <162022871+drosproos@users.noreply.github.com>
2025-07-11 18:18:43 +0500

    Create start.sh

---
+++ server/start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Downloading PaperMC..."
+curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/123/downloads/paper-1.20.1-123.jar
+java -Xmx1024M -Xms1024M -jar paper.jar nogui
