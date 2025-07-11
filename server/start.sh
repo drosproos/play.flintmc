bd35b3a08b496ef367896010115d28d8042b545a
drosproos <162022871+drosproos@users.noreply.github.com>
2025-07-11 19:04:56 +0500

    Update start.sh

--- server/start.sh
+++ server/start.sh
@@ -2,3 +2,7 @@
 echo "Downloading PaperMC..."
 curl -o paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/123/downloads/paper-1.20.1-123.jar
 java -Xmx1024M -Xms1024M -jar paper.jar nogui
+# Trigger rebuild
+git add .
+git commit -m "Trigger rebuild"
+git push
